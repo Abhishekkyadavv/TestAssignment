fb50239f31ed136819ff655819d8f6e4f7e86ee8
Abhishek yadav <abhishekyadav@iesl.co>
2023-07-21 12:02:41 +0530

    assignmentTest

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react';
+
 
 function App() {
+const [taskAdd,setTaskAdd]=useState([]);
+const [newTask,setNewTask]=useState('');
+const HandleInput=(e)=>{
+  console.log(e.target.value)
+  setNewTask(e.target.value);
+
+}
+const AddTask=()=>{
+// if(newTask.trim() !==''){
+  setTaskAdd([...taskAdd,{text:newTask,complete:false,id:Date.now()}])
+  setNewTask('');
+// }
+}
+const HandleDelete=(taskId)=>{
+  const UpdateTask=taskAdd.filter((taskIs)=>taskIs.id != taskId)
+  setTaskAdd(UpdateTask);
+
+}
+console.log(taskAdd);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <>
+<h1>Todo App</h1>
+<input type='text' value={newTask} onChange={(e)=>HandleInput(e)}/>
+<button onClick={()=>AddTask()}>Add</button>
+
+<ul>
+ {taskAdd && taskAdd.map((val)=>{
+ return  <li key={val.id} className='styleRemove'>
+ <span className='textStyle'> {val.text}</span><button onClick={()=>HandleDelete(val.id)}>X</button>
+  </li>
+ })} 
+</ul>
+  </>
   );
 }
 
